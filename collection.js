afdac75b36b4da846fbd1171f2c2f38a0be9a672
agkabongo <agkabongo@LAPTOP-AGK.localdomain>
2020-11-12 10:31:25 +0100

    js file

---
+++ collection.js
@@ -0,0 +1,85 @@
+let collection = [
+    { 
+        title: 'How to get away with murder',
+        producteur: 'Peter Nowalk',
+        category: ['Crime TV Dramas','Suspense'],
+        link:'https://www.netflix.com/title/80024057',
+        description:'A suspense-driven legal thriller about a group of ambitious law students and their mysterious criminal defense professor. They become entangled in a murder plot that will shake the entire university.'
+    },
+    { 
+        title:'Blacklist',
+        producteur: 'Jon Bokenkamp',
+        category:['Crime TV Dramas','Exciting'],
+        link:'https://www.netflix.com/title/70281312',
+        description:'A new FBI profiler, Elizabeth Keen, has her entire life uprooted when a mysterious criminal, Raymond Reddington, who has eluded capture for decades, turns himself in and insists on speaking only to her.'
+    },
+
+    { 
+        title:'Punisher',
+        producteur: 'Steve Lightfoot',
+        category:['TV Thrillers','Forceful'],
+        link:'https://www.netflix.com/title/80117498',
+        description:"A former Marine out to punish the criminals responsible for his family's murder find himself ensnared in a military conspiracy"
+    },
+    { 
+        title:'Caliphate',
+        producteur: 'Wilhelm Behrman',
+        category:['TV Dramas','Suspenseful'],
+        link:'https://www.netflix.com/title/80240005',
+        description:'Agent Fatima gets a tip that a terrorist act is planned in Sweden. At the same time, the teenager Sulle has opened her eyes to her student assistant who opens the doors to a new fascinating world.'
+    },
+
+    { 
+        title:'Get organized',
+        producteur: ['Clea Shearer', 'Joanna Teplin'],
+        category:['Lifestyle','Feel-Good'],
+        link:'https://www.netflix.com/title/81094723',
+        description:'Expert home organizers Clea and Joanna help celebrities and everyday clients, edit,categorize and contain their clutter to create stunning spaces'
+
+    },
+    { 
+        title:'Outside Man',
+        producteur: 'Reggie Yates',
+        category:['Docuseries','Investigative'],
+        link:'https://www.netflix.com/title/80191975',
+        description:'Award-winning filmmaker Reggie Yates travels around the world tackling big issues such as gun violence, racism, gay rights and addiction'
+
+
+    },
+    { 
+        title:'Greenleaf',
+        producteur: 'Craig Wright',
+        category:['TV Dramas','Emotional'],
+        link:'https://www.netflix.com/title/80140955',
+        description:'Follow the story of the eponymous family, who run a megachurch in Memphis, Tennessee with a largely African-American membership base.'
+
+
+    },
+    { 
+        title:'Top Boy',
+        producteur: 'Ronan Benett',
+        category:['Crime TV Dramas','Gritty'],
+        link:'https://www.netflix.com/title/80217669',
+        description:'In the courtyards of the Summerhouse Estate, a thriving but underground drug business is being run by Dushane and his friend Sully'
+
+    },
+    { 
+        title:'Abstract The art of Design',
+        producteur: 'Scott Dadish',
+        category:['Lifestyle','Inspiring'],
+        link:'https://www.netflix.com/title/80057883',
+        description:'Step inside the minds of the most innovative designers in a variety of disciplines and learn how design impacts every aspect of life.'
+
+
+    },
+    { 
+        title:'Dynasty',
+        producteur: ['Sallie Patrick', 'Josh Schwartz'],
+        category:['TV Dramas','Gossip'],
+        link:'https://www.netflix.com/title/80179394',
+        description:"Follows two of America's wealthiest families as they feud for control over their fortune and their children."
+
+
+    },
+   
+]
